324e5a5783c3ebbac1f28a86e856f2f6f1cec55c
Thanh Tu <thanhthanhvava2004@gmail.com>
2024-05-03 21:13:21 +0700

    adding folder (ctn)

--- PBL3/PBL3/Form1.Designer.cs
+++ PBL3/PBL3/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PBL3
 {
-    partial class Form1
+    partial class HomeForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // HomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1267, 636);
+            this.MinimumSize = new System.Drawing.Size(1285, 683);
+            this.Name = "HomeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Home Form";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
