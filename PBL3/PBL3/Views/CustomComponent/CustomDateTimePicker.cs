cb1103bc850ea041222333baeda6bc32606ea69b
Thanh Tu <thanhthanhvava2004@gmail.com>
2024-05-04 20:05:46 +0700

    add forms

---
+++ PBL3/PBL3/Views/CustomComponent/CustomDateTimePicker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace PBL3.Views.CustomComponent
+{
+    public class CustomDateTimePicker : DateTimePicker
+    {
+    }
+}
